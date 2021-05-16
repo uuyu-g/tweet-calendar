ddf59ae0dcd9fb4b3da935736e793c57b656c18c
yutaro <aqueous.mkli@gmail.com>
2021-05-16 21:50:57 +0900

    add icons.tsx

---
+++ src/icons.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export function ArrowLeft() {
+  return (
+    <svg
+      className="h-6 leading-none text-gray-500 w-6 inline-flex"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  )
+}
+export function ArrowRight() {
+  return (
+    <svg
+      className="h-6 leading-none text-gray-500 w-6 inline-flex"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  )
+}
